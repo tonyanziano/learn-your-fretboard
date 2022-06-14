d9bade02daabd14d1c5080d18e52830deba2901b
Tony <tonyanziano5@gmail.com>
2022-06-13 22:26:47 -0700

    Laid out dev infrastructure

---
+++ webpack.config.prod.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index-bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/index.html')
+  })],
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
