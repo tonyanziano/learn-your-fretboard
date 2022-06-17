967b898bb6c0c2821716829e32b8cb1a45f4ffe9
Tony <tonyanziano5@gmail.com>
2022-06-16 22:01:38 -0700

    Fixed naming of the accidental notes

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,3 @@
-export const NaturalNotes = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G'
-];
+export const NaturalNotes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-export const ChromaticNotes = [
-  'A#',
-  'B#',
-  'D#',
-  'F#',
-  'G#'
-];
+export const AccidentalNotes = ['A#', 'B#', 'D#', 'F#', 'G#']
