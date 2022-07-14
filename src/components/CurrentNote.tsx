d0f4eeda451b4da3152c357483c39ee40aa31169
Tony <tonyanziano5@gmail.com>
2022-07-13 20:02:02 -0700

    Started styling current note display

--- src/components/CurrentNote.tsx
+++ src/components/CurrentNote.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useCurrentNote } from '../hooks/useCurrentNote';
+/** @jsx jsx **/
+import { css } from '@emotion/react';
+
+const styles = css({
+  padding: '8px 0',
+  textAlign: 'center',
+  fontSize: 72,
+});
 
 /**
  * Displays the current note that should be played by the user
  */
-export const CurrentNote: React.FC<{}> = () => {
+export const CurrentNote: React.FC = () => {
   const currentNote = useCurrentNote();
 
-  return <div>{currentNote}</div>;
+  return <div css={styles}>{currentNote}</div>;
 };
