80777232fddeff7ed0b49cbfad37f1fa6f1bdedc
Tony <tonyanziano5@gmail.com>
2022-07-13 19:53:48 -0700

    Leaned down <CurrentNote/>

--- src/components/CurrentNote.tsx
+++ src/components/CurrentNote.tsx
@@ -1,9 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { NaturalNotes } from '../constants';
+import React from 'react';
 import { useCurrentNote } from '../hooks/useCurrentNote';
-
-const bpm = 40;
-const bpmInMs = (60 / bpm) * 1000;
 
 /**
  * Displays the current note that should be played by the user
